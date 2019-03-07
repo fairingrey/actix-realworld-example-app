1f9e7b57b73beecfb8b57d46e676cf1abb36c03f
grey <fairingrey@gmail.com>
2019-03-06 18:48:28 -0800

    change unique violation error to 422 unprocessable

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,10 @@
 use actix_web::{
     actix::MailboxError,
-    error::ResponseError,
+    error::{
+        self,
+        ResponseError,
+    },
+    http::StatusCode,
     HttpResponse,
 };
 use diesel::{
@@ -17,16 +21,25 @@ use validator::{
     ValidationErrors
 };
 use std::convert::From;
+use actix_web::ws::start;
 
 // more error types can be found at below link but we should only need these for now
 // https://actix.rs/actix-web/actix_web/struct.HttpResponse.html
 #[derive(Fail, Debug)]
 pub enum Error {
+
+    // 400
+    #[fail(display = "Bad Request: {}", _0)]
+    BadRequest(String),
+
+    // 422
+    #[fail(display = "Unprocessable Entity: {}", _0)]
+    UnprocessableEntity(String),
+
+    // 500
     #[fail(display = "Internal Server Error")]
     InternalServerError,
 
-    #[fail(display = "Bad Request: {}", _0)]
-    BadRequest(String),
 }
 
 // the ResponseError trait lets us convert errors to http responses with appropriate data
@@ -34,10 +47,11 @@ pub enum Error {
 impl ResponseError for Error {
     fn error_response(&self) -> HttpResponse {
         match *self {
+            Error::BadRequest(ref message) => HttpResponse::BadRequest().json(message),
+            Error::UnprocessableEntity(ref message) => HttpResponse::build(StatusCode::UNPROCESSABLE_ENTITY).json(message),
             Error::InternalServerError => {
                 HttpResponse::InternalServerError().json("Internal Server Error")
             },
-            Error::BadRequest(ref message) => HttpResponse::BadRequest().json(message),
         }
     }
 }
@@ -60,7 +74,7 @@ impl From<DieselError> for Error {
             DieselError::DatabaseError(kind, info) => {
                 if let DatabaseErrorKind::UniqueViolation = kind {
                     let message = info.details().unwrap_or_else(|| info.message()).to_string();
-                    return Error::BadRequest(message);
+                    return Error::UnprocessableEntity(message);
                 }
                 Error::InternalServerError
             }
