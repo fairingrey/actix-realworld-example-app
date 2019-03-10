3396cfd2433036620600a3babc973f023510b8a5
grey <fairingrey@gmail.com>
2019-03-10 11:44:14 -0700

    add this constraint in db also

--- migrations/2019-03-06-144624_create_followers/up.sql
+++ migrations/2019-03-06-144624_create_followers/up.sql
@@ -12,4 +12,8 @@ CREATE TABLE followers (
 CREATE INDEX followers_user_id_idx ON followers (user_id);
 CREATE INDEX followers_follower_user_id_idx ON followers (follower_id);
 
+-- also prevent users from following themselves
+-- this should be caught in application logic and return a 422, but if it doesn't...
+ALTER TABLE followers ADD CONSTRAINT user_id_cannot_be_equal_to_follower_id_chk CHECK (user_id != follower_id);
+
 SELECT diesel_manage_updated_at('followers');
