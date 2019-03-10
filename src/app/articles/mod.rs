8a8814b203e5e0ab61876decd9dc5d33e9cae29e
grey <fairingrey@gmail.com>
2019-03-10 15:06:47 -0700

    I should really squash this but w/e

--- src/app/articles/mod.rs
+++ src/app/articles/mod.rs
@@ -35,8 +35,8 @@ pub struct ArticlesParams {
 
 #[derive(Debug, Deserialize)]
 pub struct FeedParams {
-    limit: Option<u32>,
-    offset: Option<u32>,
+    limit: Option<usize>,
+    offset: Option<usize>,
 }
 
 // Client Messages ↓
