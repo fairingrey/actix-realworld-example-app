7dabcbd09bb4aa5a30ec9c74b26d5424049f6650
grey <fairingrey@gmail.com>
2019-03-15 09:29:58 -0700

    use HASHER object as pub static reference

--- src/utils/hasher.rs
+++ src/utils/hasher.rs
@@ -3,13 +3,13 @@ use libreauth::pass::{Algorithm, HashBuilder, Hasher};
 pub const PWD_ALGORITHM: Algorithm = Algorithm::Argon2;
 pub const PWD_SCHEME_VERSION: usize = 1;
 
-// This should never fail, unless it's given invalid parameters
-// If the HashBuilder changes, make sure to increment PWD_SCHEME_VERSION
-pub fn hasher() -> Hasher {
-    // hasher configuration specifics
-    HashBuilder::new()
-        .algorithm(PWD_ALGORITHM)
-        .version(PWD_SCHEME_VERSION)
-        .finalize()
-        .unwrap()
+// If the Hasher changes, make sure to increment PWD_SCHEME_VERSION
+lazy_static! {
+    pub static ref HASHER: Hasher = {
+        HashBuilder::new()
+            .algorithm(PWD_ALGORITHM)
+            .version(PWD_SCHEME_VERSION)
+            .finalize()
+            .unwrap()
+    };
 }
